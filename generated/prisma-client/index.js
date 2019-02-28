5ecb88866f6a95241e6af7bbb2a25452f3594f2d
chicrock <jungsungrak@gmail.com>
2019-02-28 17:08:07 +0900

    Add graphql get-schema

--- generated/prisma-client/index.js
+++ generated/prisma-client/index.js
@@ -16,6 +16,6 @@ var models = [
 exports.Prisma = prisma_lib_1.makePrismaClientClass({
   typeDefs,
   models,
-  endpoint: `https://us1.prisma.sh/rock-dc28b2/prisma-example/dev`
+  endpoint: `http://localhost:4466`
 });
 exports.prisma = new exports.Prisma();
